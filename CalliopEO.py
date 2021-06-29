a1b85a6073d7563e2a6294ba634003f4e6ea1ea6
Jørn Alraun <ja@urbn-pockets.com>
2021-06-29 11:44:30 +0200

    Initial commit

---
+++ CalliopEO.py
@@ -0,0 +1,276 @@
+import os
+import shutil
+import datetime
+import glob
+import sys
+import serial
+import time
+#import blkinfo
+
+#blk = blkinfo.BlkDiskInfo()
+
+#TEMP_MOUNT_MINI = "/tmp/mini"
+TEMP_MOUNT_MINI = "/home/calliope/mnt/mini"
+#TEMP_MOUNT_FLASH = "/tmp/flash"
+TEMP_MOUNT_FLASH = "/home/calliope/mnt/flash"
+
+#MODEL_MINI_VALUE = "SEGGER MSD Volume"
+#MODEL_FLASH_VALUE = "SEGGER MSD FLASH"
+
+#MSG_RUN_AS_ROOT = "please run as root"
+MSG_MINI_NOT_FOUND = "mini not found"
+MSG_FLASH_NOT_FOUND = "flash not found"
+MSG_MINI_NO_SERIAL = "no serial connection found"
+
+#CMD_MOUNT = "mount -t vfat %s %s"
+CMD_MOUNT = "mount %s"
+CMD_UNMOUNT = "umount %s"
+CMD_SYNC = "sync %s"
+
+#define the archive ending you want to search for
+archive_ending = ".zip"
+
+DEFAULT_ENCODING = "utf-8"
+SERIAL_START = "@START@"
+SERIAL_END = "@END@"
+SERIAL_TIMEOUT = 10
+
+def getMiniSerial():
+    devices = glob.glob("/dev/ttyACM*")
+    if  len(devices) > 0:
+        return devices[0]  # should only every be one
+    else:
+        return None
+
+ser = serial.Serial(
+    port=getMiniSerial(),
+    baudrate=115200,
+    bytesize=serial.EIGHTBITS,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    timeout=10)
+
+def serialConnect():
+    port = getMiniSerial()
+    #if ser.getPort() != port:
+    if ser.port != port:
+        ser.close()
+        ser.port=port
+        ser.baudrate=115200
+        ser.bytesize=serial.EIGHTBITS
+        ser.parity=serial.PARITY_NONE
+        ser.stopbits=serial.STOPBITS_ONE
+        ser.timeout=10
+        ser.open()
+
+def safe_decode(bytes, encoding=DEFAULT_ENCODING):
+    try:
+        return bytes.decode(encoding).strip()
+    except:
+        return ""
+
+#waits for SERIAL_START
+#if a timeout is reached the return value is False
+def waitSerialStart():
+    serialTime = time.time()
+    line = ""
+    ser.write(b'@START@\r\n')
+    while True:
+        line = safe_decode(ser.readline())
+        if SERIAL_START in line:
+            return True
+        elif (time.time() - serialTime) > SERIAL_TIMEOUT:
+            return False
+
+#reads the data received from mini ans returns it
+#if SERIAL_END is received True is returnes indicating the end
+def readSerialUntilEnd():
+    line = ""
+    while True:
+        line = safe_decode(ser.readline())
+        if SERIAL_END in line:
+            return True
+        else:
+            return line
+
+#waits for SERIAL_START and collects the data received from mini until SERIAL_END is received
+#if a timeout is received the return value is False
+def readSerialData():
+    lines = []
+    ans = waitSerialStart()
+    if ans == True:
+        while True:
+            ans = readSerialUntilEnd()
+            if ans == True:
+                print("\r\n" + str(len(lines)) + " lines read")
+                return lines
+            else:
+                lines.append(ans)
+                print("*",end="",flush=True)
+    elif ans == False:
+        return False
+
+def listFolders(path):
+    temp_list = []
+    for folder in os.listdir(path):
+        if os.path.isdir(os.path.join(path, folder)):
+            temp_list.append(os.path.join(path, folder))
+    return temp_list
+
+#unpacks given archives in a folder and returns the foldername
+def unpackArchives(archive_list, destname=None):
+    if(destname == None):
+        archive_folder = os.path.join(
+                os.getcwd(),
+                datetime.datetime.now().strftime("run_%d%m%y-%H%M")
+                )
+    else:
+        archive_folder = os.path.join(os.getcwd(),destname)
+
+    for file in archive_list:
+        shutil.unpack_archive(file, archive_folder)
+        shutil.move(file, file + ".done")
+    return archive_folder
+
+#list files in this path filtered by ending and recurse if desired
+def listFiles(path, ending=None, recurse=False):
+    temp_list = []
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path,file)):
+            if ending == None:
+                temp_list.append(os.path.join(path,file))
+            else:
+                if file.endswith(ending):
+                    temp_list.append(os.path.join(path,file))
+        else:
+            if recurse and os.path.isdir(os.path.join(path,file)):
+                temp_list.extend(listFiles(os.path.join(path,file), ending))
+    return temp_list
+
+#def getMiniDisk():
+#    MINI_DEVICE = ""
+#    disks = blk.get_disks()
+#    for disk in disks:
+#        #print(disk['model'])
+#        if disk['model'] == MODEL_MINI_VALUE:
+#            MINI_DEVICE = "/dev/" + disk['name']
+#            return MINI_DEVICE
+#    return None
+#
+#def getFlashDisk():
+#    FLASH_DEVICE = ""
+#    disks = blk.get_disks()
+#    for disk in disks:
+#        if disk['model'] == MODEL_FLASH_VALUE:
+#            FLASH_DEVICE = "/dev/" + disk['name']
+#            return FLASH_DEVICE
+#    return None
+
+#programm mini
+def programmMini(hex):
+    #mount mini disk
+    #os.system(CMD_MOUNT % (getMiniDisk(), TEMP_MOUNT_MINI))
+    os.system(CMD_MOUNT % (TEMP_MOUNT_MINI))
+    #programm mini
+    shutil.copy2(hex, TEMP_MOUNT_MINI)
+    os.system(CMD_SYNC % TEMP_MOUNT_MINI)
+    os.system(CMD_UNMOUNT % TEMP_MOUNT_MINI)
+    #wait for mini disconect and reconnect
+    time.sleep(20)
+    #connect to serial
+    serialConnect()
+
+def writeToFile(hex, data):
+    file = open(hex+".data","w")
+    for line in data:
+        file.write(line+"\r\n")
+    file.close()
+
+###################################################
+
+def main():
+    #we need root
+#    if os.geteuid() != 0:
+#        print(MSG_RUN_AS_ROOT)
+#        sys.exit(0)
+    #check mini disk
+#    if getMiniDisk() == None:
+#        print(MSG_MINI_NOT_FOUND)
+#        sys.exit(0)
+    #check flash disk
+#    if getFlashDisk() == None:
+#        print(MSG_FLASH_NOT_FOUND)
+#        sys.exit(0)
+    #check serial
+    if getMiniSerial() == None:
+        print(MSG_MINI_NO_SERIAL)
+        sys.exit(0)
+    #make mini mount dir
+    if not os.path.exists(TEMP_MOUNT_MINI):
+        os.mkdir(TEMP_MOUNT_MINI)
+    #make flash mount dir
+    if not os.path.exists(TEMP_MOUNT_FLASH):
+        os.mkdir(TEMP_MOUNT_FLASH)
+
+    #get archives
+    archive_file_list = listFiles(os.getcwd(), ending=archive_ending)
+    if len(archive_file_list) == 0:
+        print("no archives found in this directory")
+        print("make sure you have at least one archive in this directory")
+        sys.exit(0)
+    #unpack archives to a folder named with current date and time
+    folder_name = unpackArchives(archive_file_list)
+    #recursively search for .hex files
+    hex_files = listFiles(folder_name, ending=".hex" ,recurse=True)
+    print("\r\n")
+
+    for hex in hex_files:
+        if os.path.exists(hex + ".data"):
+            print("skipping: " + hex)
+            continue
+        tries  = 1
+        while True:
+            print("programming: " + hex)
+            programmMini(hex)
+            print("done")
+            print("reading data")
+            data = readSerialData()
+
+            if data == False:
+                print("Something went wrong retrying: " + str( tries ) + "/5")
+                tries = tries + 1
+                if tries > 5:
+                    #give up
+                    break
+                else:
+                    #repeat programming
+                    continue
+            else:
+                writeToFile(hex, data)
+                print("done")
+                print("############################################################################################################\r\n")
+                break
+
+if __name__ == "__main__":
+    main()
+
+#TODO
+## programm
+## safe to file
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
