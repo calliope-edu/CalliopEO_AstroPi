c7364e05994f0664c515ab241adc09d64d059d84
Jan Grosser <email@jan-grosser.de>
2021-07-08 18:27:42 +0200

    Added very first test scripts

---
+++ testing.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# This bash script is used to perform testing on the
+# Python script CalliopEO.py.
+
+# If exists, source the file testing.conf which holds
+# definitions for some variables.
+testing_conf="./testing.conf"
+if [[ -f "${testing_conf}" ]]; then
+    source ${testing_conf}
+fi
+
+# All the testcases are defined as bash scripts in the folder
+# ${tc_folder}. If ${tc_folder} is unset (equal empty string) set it to
+# default
+if [[ ${tc_folder} == "" ]]; then
+    tc_folder="./testcases"
+fi
+
+echo "################## TESTING CALLOPEO.PY ##################"
+echo "Test started: $(date)"
+
+# Loop over all testcases found in the folder ${tc_folder}
+for testcase in ${tc_folder%/}/*.*; do
+    # If the variable ${tc_confirm} is set to true ask
+    # before execute any testcase. If the variable is unset or set
+    # to false, execute testcase immediately.
+    if [[ ${tc_confirm} == "true" ]]; then
+        read -p "Execute testcase \"$(basename ${testcase})\"? [y/N] " ans
+    else
+        ans="y"
+    fi
+    if [[ ${ans} =~ [Yy]$ ]]; then
+        echo ""
+        source ${testcase}
+        echo ""
+    fi
+done
+
+echo "Test ended: $(date)"
+exit 0
+
