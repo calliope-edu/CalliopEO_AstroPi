16d1ec1b25898c6e925a3317cce90714ffe08e81
JW <gitkraken@juriwolf.de>
2021-08-11 12:05:17 +0200

    remove files not needed in flight

--- testcases/testfiles/no-start.js
+++
@@ -1,9 +0,0 @@
-let runProgram = true
-basic.forever(function () {
-    images.iconImage(IconNames.Confused).showImage(0)
-    // Student code goes here.
-    while (runProgram) {
-        serial.writeLine("plzkillme!")
-    }
-})
-
